d09acdcd5b214c672dc201dafdf40117f01ee290
StuartCox3107 <coxstuart@hotmail.com>
2020-11-22 21:04:03 +0000

    test add

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+   os.environ.setdefault("SECRET_KEY", "whsec_5AnJ2pC6bzEIjOK4yRK8YGrwTuOp2BM8")
+   os.environ.setdefault("DATABASE_URL", "postgres://xjnsuqzmrgzkoh:aa44297a93a6344e5b679fa53a95a0a268e6dc95f41ac46dc5a7a1a32f335a2e@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/da08vfrkvqufng")
+   os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "bup9lstnAarrWhaSdOqszfbOGEB1ynjXllm2UPa9")
+   os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAUFZDWG2VTMKLW56P")
