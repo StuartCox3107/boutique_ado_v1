5462915c0e5ba8859914604fea9573b04e56e493
StuartCox3107 <coxstuart@hotmail.com>
2020-11-24 16:10:19 +0000

    heroku push attempt tues 1610

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-   os.environ("SECRET_KEY", "whsec_5AnJ2pC6bzEIjOK4yRK8YGrwTuOp2BM8")
-   os.environ("DATABASE_URL", "postgres://xjnsuqzmrgzkoh:aa44297a93a6344e5b679fa53a95a0a268e6dc95f41ac46dc5a7a1a32f335a2e@ec2-54-75-231-215.eu-west-1.compute.amazonaws.com:5432/da08vfrkvqufng")
-   os.environ("AWS_SECRET_ACCESS_KEY", "bup9lstnAarrWhaSdOqszfbOGEB1ynjXllm2UPa9")
-   os.environ("AWS_ACCESS_KEY_ID", "AKIAUFZDWG2VTMKLW56P")
